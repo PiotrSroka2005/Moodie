9081689fd82007e2d0c90c34705cb5bc47e7c5a9
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-01-31 10:34:30 +0100

    Add database with functions to operate moods

---
+++ Moodie/Moodie/Database.cs
@@ -0,0 +1,39 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moodie
+{
+   public class Database
+   {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Humor>();
+        }
+
+        public Task<int> InsertDayMoodAsync(Humor)
+        {
+            return _database.InsertAsync(Humor);
+        }
+
+        public Task<Humor> GetLastDayMood()
+        {
+            return _database.Table<Humor>().Where(d => d.Date != DateTime.Now.Date).OrderByDescending(d => d.Date).FirstOrDefaultAsync();
+        }
+
+        public Task<Humor> GetDayMood(DateTime dateTime)
+        {
+            return _database.Table<Humor>().Where(d => d.Date == dateTime).FirstOrDefaultAsync();
+        }
+
+        public Task<List<Humor>> GetDayMoods()
+        {
+            return _database.Table<Humor>().OrderByDescending(d => d.Date).ToListAsync();
+        }
+   }
+}
