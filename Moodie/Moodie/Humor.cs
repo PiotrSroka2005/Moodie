e2e90f1dcb7034f75e3fec87038c0aac3a694953
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-01-31 10:36:47 +0100

    Add class with mood data

---
+++ Moodie/Moodie/Humor.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moodie
+{
+    public class Humor
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public MoodEnum Humor { get; set; }
+    }
+}
